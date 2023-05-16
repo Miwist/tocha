b458e39b8db056717dd48ca743f99010d7f32f84
mikhail <mikhail@Mac-mini.local>
2023-05-16 16:59:28 +0600

    fix elements css

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "./Reviews.module.scss";
 import photo_reviews_1 from "../../assets/img/photo_reviews_1.png";
 import photo_reviews_2 from "../../assets/img/photo_reviews_2.png";
 
 const Reviews = () => {
+  const [sliderCurrent, setSliderCurrent] = useState(0);
+  const [road, setRoad] = useState(0);
   const reviews_arr = [
     {
       id: 1,
@@ -20,62 +22,37 @@ const Reviews = () => {
       name: "Андрей Васильев",
       title: "Теперь мы идем в ногу со временем",
       text: `А ещё интерактивные прототипы превращены в посмешище, хотя само их существование приносит несомненную пользу обществу. Банальные, но неопровержимые выводы, а также тщательные исследования конкурентов формируют сеть.`,
-    },
-    {
-      id: 3,
-      name: "Анна Купер",
-      web: "romashka.ru",
-      img: photo_reviews_1,
-      title: "Как же удобно!",
-      text: `В рамках спецификации современных стандартов, базовые сценарии поведения пользователей, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут преданы социально-демократической анафеме.`,
-    },
-    {
-      id: 4,
-      name: "Андрей Васильев",
-      web: "samogon-ont.ru",
-      img: photo_reviews_2,
-      title: "Как же удобно!",
-      text: `В рамках спецификации современных стандартов, базовые сценарии поведения пользователей, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут преданы социально-демократической анафеме.`,
-    },
-    {
-      id: 5,
-      name: "Анна Купер",
-      web: "romashka.ru",
-      img: photo_reviews_1,
-      title: "Как же удобно!",
-      text: `В рамках спецификации современных стандартов, базовые сценарии поведения пользователей, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут преданы социально-демократической анафеме.`,
-    },
-    {
-      id: 6,
-      name: "Андрей Васильев",
-      img: photo_reviews_2,
-      web: "samogon-ont.ru",
-      title: "Как же удобно!",
-      text: `В рамках спецификации современных стандартов, базовые сценарии поведения пользователей, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут преданы социально-демократической анафеме.`,
-    },
+    }
   ];
+
+  function moveSlider(id) {
+    setSliderCurrent(id);
+  }
 
   return (
     <section className={cl.Reviews}>
       <h3>Отзывы клиентов</h3>
       <div className={cl.Reviews_items}>
-       
-        {reviews_arr.map((item) => (
-          <div className={cl.Reviews_items__item}>
+               
+          <div className={cl.Reviews_items__item}  >
             <div className={cl.Reviews_items__item__text}>
-              <p>{item.title}</p>
-              <i>{item.text}</i>
+              <p>{reviews_arr[sliderCurrent].title}</p>
+              <i>{reviews_arr[sliderCurrent].text}</i>
             </div>
             <div className={cl.Reviews_items__item__img}>
-              <img src={item.img} alt={item.name} />
+              <img src={reviews_arr[sliderCurrent].img} alt={reviews_arr[sliderCurrent].name} />
               <div className={cl.Reviews_items__item__img_name}>
-                <p>{item.name}</p>
-                <i>{item.web}</i>
+                <p>{reviews_arr[sliderCurrent].name}</p>
+                <i>{reviews_arr[sliderCurrent].web}</i>
               </div>
             </div>
           </div>
+      </div>
+      <div className={cl.dots}>
+        {reviews_arr.map((item) => (
+         
+          <span className={cl.dot} onClick={() => moveSlider(item.id - 1)}></span>
         ))}
-  
       </div>
     </section>
   );
